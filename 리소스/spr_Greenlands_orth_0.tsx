866888a531a970a8080b48f75a2911fa216d48a6
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2024-06-16 22:46:08 +0900

    맵 리소스 추가

---
+++ 리소스/spr_Greenlands_orth_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="spr_Greenlands_orth_0" tilewidth="16" tileheight="16" tilecount="91" columns="13">
+ <image source="../../다운로드/spr_Greenlands_orth_0.png" width="208" height="112"/>
+</tileset>
