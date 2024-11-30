520533c5e65933e4fb61367568575853a0067bd3
Lee Jong Hyeok <gurpaper@naver.com>
2024-11-30 18:53:37 +0900

    Update server.cpp

--- Server/server.cpp
+++ Server/server.cpp
@@ -224,6 +224,7 @@ void get_new_client_id(int& id)
 	for (int i = 0; i < MAX_USER; ++i) {
 		lock_guard <mutex> ll{ clients[i].s_lock_ };
 		if (clients[i].state_ == ST_FREE) {
+			clients[i].state_ = ST_ALLOC;
 			id = i;
 			return;
 		}
@@ -245,10 +246,69 @@ void process_packet(int c_id, char* packet)
 	switch (packet[2]) {
 	case CS_LOGIN: {
 		CS_LOGIN_PACKET* p = reinterpret_cast<CS_LOGIN_PACKET*>(packet);
-		clients[c_id].login_id_ = p->id;
 		strcpy_s(clients[c_id].name_, p->name);
 		clients[c_id].db_state_ = 1;
 		db_queue.push(c_id);
+		while (clients[c_id].db_state_ == 1) {}
+
+		clients[c_id].pos_.id_ = c_id;
+		int character = rand() % 3;
+		if (character == 0) {
+			clients[c_id].character_ = WARRIOR;
+			clients[c_id].damage_ = WARRIOR_STAT_ATK * clients[c_id].level_;
+			clients[c_id].armor_ = WARRIOR_STAT_ARMOR * clients[c_id].level_;
+		}
+		else if (character == 1) {
+			clients[c_id].character_ = MAGE;
+			clients[c_id].damage_ = MAGE_STAT_ATK * clients[c_id].level_;
+			clients[c_id].armor_ = MAGE_STAT_ARMOR * clients[c_id].level_;
+		}
+		else if (character == 2) {
+			clients[c_id].character_ = PRIST;
+			clients[c_id].damage_ = PRIST_STAT_ATK * clients[c_id].level_;
+			clients[c_id].armor_ = PRIST_STAT_ARMOR * clients[c_id].level_;
+		}
+		clients[c_id].state_ = ST_INGAME;
+
+		clients[c_id].in_use_ = true;
+		clients[c_id].send_login_info_packet();
+		Sector.addObject(clients[c_id].pos_);
+
+		unordered_set <int> vl;
+		Sector.getNearbyObjects(vl, clients[c_id].pos_);
+		unordered_set <int> new_vl;
+		for (auto& pl : vl) {
+			if (clients[pl].in_use_ == false) continue;
+			if (pl == c_id) continue;
+			if (clients[pl].state_ != ST_INGAME) continue;
+			if (false == can_see(pl, c_id)) continue;
+			new_vl.insert(pl);
+		}
+
+		clients[c_id].vl_.lock();
+		unordered_set<int> old_vlist = clients[c_id].view_list_;
+		clients[c_id].vl_.unlock();
+
+		for (auto pl : new_vl) {
+			auto& cpl = clients[pl];
+			if (is_pc(pl)) {
+				cpl.vl_.lock();
+				if (clients[pl].view_list_.count(c_id)) {
+					cpl.vl_.unlock();
+					clients[pl].send_move_packet(c_id);
+				}
+				else {
+					cpl.vl_.unlock();
+					clients[pl].send_add_object_packet(c_id);
+				}
+			}
+
+			if (old_vlist.count(pl) == 0) {
+				clients[c_id].send_add_object_packet(pl);
+				if (false == is_pc(pl))
+					WakeUpNPC(pl);
+			}
+		}
 		break;
 	}
 	case CS_MOVE: {
@@ -890,10 +950,6 @@ void worker_thread(HANDLE h_iocp)
 			int client_id;
 			get_new_client_id(client_id);
 			if (client_id != -1) {
-				{
-					lock_guard<mutex> ll(clients[client_id].s_lock_);
-					clients[client_id].state_ = ST_ALLOC;
-				}
 				clients[client_id].pos_.x_ = 0;
 				clients[client_id].pos_.y_ = 0;
 				clients[client_id].pos_.id_ = client_id;
@@ -992,67 +1048,6 @@ void worker_thread(HANDLE h_iocp)
 			}
 			break;
 		}
-		case OP_LOGIN: {
-			clients[key].pos_.id_ = key;
-			int character = rand() % 3;
-			if (character == 0) {
-				clients[key].character_ = WARRIOR;
-				clients[key].damage_ = WARRIOR_STAT_ATK * clients[key].level_;
-				clients[key].armor_ = WARRIOR_STAT_ARMOR * clients[key].level_;
-			}
-			else if (character == 1) {
-				clients[key].character_ = MAGE;
-				clients[key].damage_ = MAGE_STAT_ATK * clients[key].level_;
-				clients[key].armor_ = MAGE_STAT_ARMOR * clients[key].level_;
-			}
-			else if (character == 2) {
-				clients[key].character_ = PRIST;
-				clients[key].damage_ = PRIST_STAT_ATK * clients[key].level_;
-				clients[key].armor_ = PRIST_STAT_ARMOR * clients[key].level_;
-			}
-			clients[key].state_ = ST_INGAME;
-
-			clients[key].in_use_ = true;
-			clients[key].send_login_info_packet();
-			Sector.addObject(clients[key].pos_);
-
-			unordered_set <int> vl;
-			Sector.getNearbyObjects(vl, clients[key].pos_);
-			unordered_set <int> new_vl;
-			for (auto& pl : vl) {
-				if (clients[pl].in_use_ == false) continue;
-				if (pl == key) continue;
-				if (clients[pl].state_ != ST_INGAME) continue;
-				if (false == can_see(pl, key)) continue;
-				new_vl.insert(pl);
-			}
-
-			clients[key].vl_.lock();
-			unordered_set<int> old_vlist = clients[key].view_list_;
-			clients[key].vl_.unlock();
-
-			for (auto pl : new_vl) {
-				auto& cpl = clients[pl];
-				if (is_pc(pl)) {
-					cpl.vl_.lock();
-					if (clients[pl].view_list_.count(key)) {
-						cpl.vl_.unlock();
-						clients[pl].send_move_packet(key);
-					}
-					else {
-						cpl.vl_.unlock();
-						clients[pl].send_add_object_packet(key);
-					}
-				}
-
-				if (old_vlist.count(pl) == 0) {
-					clients[key].send_add_object_packet(pl);
-					if (false == is_pc(pl))
-						WakeUpNPC(pl);
-				}
-			}
-			break;
-		}
 		case OP_LOGIN_FAIL: {
 			cout << key << " : LOGIN_ FAIL\n";
 			disconnect(static_cast<int>(key));
@@ -1118,7 +1113,7 @@ void connect_db() {
 							}
 							if (clients[userId].db_state_ == 1) {
 								wstring func = L"EXEC SearchClient ";
-								func += std::to_wstring(clients[userId].login_id_);
+								func += std::to_wstring(userId);
 								retcode = SQLExecDirect(hstmt, (SQLWCHAR*)func.c_str(), SQL_NTS);
 								if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
 									SQLBindCol(hstmt, 1, SQL_C_WCHAR, &userName, sizeof(userName), &cbName);
@@ -1139,9 +1134,7 @@ void connect_db() {
 											clients[userId].exp_ = userExp;
 											clients[userId].pos_.x_ = userX;
 											clients[userId].pos_.y_ = userY;
-											OVER_EXP* ov = new OVER_EXP;
-											ov->comp_type_ = OP_LOGIN;
-											PostQueuedCompletionStatus(h_iocp, 1, userId, &ov->over_);
+											clients[userId].db_state_ = 0;
 											break;
 										}
 										else {
@@ -1149,6 +1142,7 @@ void connect_db() {
 											OVER_EXP* ov = new OVER_EXP;
 											ov->comp_type_ = OP_LOGIN_FAIL;
 											PostQueuedCompletionStatus(h_iocp, 1, userId, &ov->over_);
+
 											//cout << clients[userId].login_id_ << " Login Fail\n";
 											break;
 										}
@@ -1160,7 +1154,7 @@ void connect_db() {
 							}
 							else if (clients[userId].db_state_ == 2) {
 								wstring func = L"EXEC SaveData ";
-								func += std::to_wstring(clients[userId].login_id_);
+								func += std::to_wstring(userId);
 								func += L", ";
 								func += std::to_wstring(clients[userId].level_);
 								func += L", ";
